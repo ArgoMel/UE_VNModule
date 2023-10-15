ce1a760c1644041d8d3ce90c13d9230d2cb53667
ArgoMel <122796999+ArgoMel@users.noreply.github.com>
2023-10-15 15:16:53 +0900

    Initial commit

---
+++ Source/VNModule/VNModule.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VNModule.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VNModule, "VNModule" );
